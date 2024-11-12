ccb6fd84c01f5520496237cca7172d04820338ef
dongHun <actgong@gmail.com>
2024-11-12 20:54:37 +0900

    first commit

---
+++ src/main/java/com/buki/log_api/LogApiApplication.java
@@ -0,0 +1,13 @@
+package com.buki.log_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buki/log_api/LogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buki.log_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
